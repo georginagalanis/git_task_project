e750aa4cb9e8bb52dc7c4ff3b32bcb642a2a657f
Georgina Galanis <georgina.galanis@gmail.com>
2024-03-25 23:24:03 +0000

    ensure names do not contain numbers

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,9 @@
+def is_valid_name(name):
+    return not any(char.isdigit() for char in name)
+
 name = input("What is your name? ")
+while (not is_valid_name(name)):
+    print("Names cannot contain numbers\n")
+    name = input("What is your name? ")
+
 print(f"Hello {name}")
